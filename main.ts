eae9d1f92a0c0b22a91d3eae484694639a03f841
Nebilam <49345234+Nebilam@users.noreply.github.com>
2019-04-28 18:23:12 +0200

    achteruit rijden

--- main.ts
+++ main.ts
@@ -10,15 +10,22 @@ basic.pause(2000)
 maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
 maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-basic.showString("Hello world! I am Laxoi micro:bit")
-basic.showString("Hoi!")
-basic.showIcon(IconNames.Happy)
-basic.pause(1000)
-basic.showIcon(IconNames.Heart)
 basic.pause(2000)
-basic.showIcon(IconNames.Happy)
-music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
-basic.pause(5000)
+if (input.buttonIsPressed(Button.A)) {
+    basic.showIcon(IconNames.Happy)
+    music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    basic.pause(5000)
+} else {
+    basic.showString("Hello world! I am Laxoi micro:bit")
+    basic.showString("Hoi!")
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.showIcon(IconNames.Happy)
+    music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    basic.pause(5000)
+}
 basic.forever(function () {
     if (crasch1 == 3 || crasch2 == 3) {
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
@@ -44,8 +51,8 @@ basic.forever(function () {
             maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            crasch1 += 1
             crasch2 = 0
+            crasch1 = 0
         }
         basic.pause(715)
     } else {
